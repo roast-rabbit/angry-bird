dba1bc05bbb7d75bb0427fe519fcd8f4c24c627a
roast-rabbit <roastrabbit@protonmail.com>
2022-11-01 15:00:42 +0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const { Engine, World, Bodies, MouseConstraint, Mouse, Constraint } = Matter;
+
+let ground;
+const boxes = [];
+let bird;
+let world, engine;
+let mConstraint;
+
+function setup() {
+  const canvas = createCanvas(600, 400);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(width / 2, height - 10, width, 20);
+  for (let i = 0; i < 3; i++) {
+    boxes[i] = new Box(450, 300 - i * 75, 50, 75);
+  }
+  bird = new Bird(50, 300, 50);
+
+  const mouse = Mouse.create(canvas.elt);
+  const options = {
+    mouse: mouse,
+  };
+
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
+}
+
+function draw() {
+  background(0);
+  Matter.Engine.update(engine);
+  ground.show();
+  for (let box of boxes) {
+    box.show();
+  }
+  bird.show();
+}
---
+++ slingshot.js
@@ -0,0 +1,22 @@
+class SlingShot {
+  constructor(x, y, body) {
+    const options = {
+      pointA: {
+        x: x,
+        y: y,
+      },
+      bodyB: body,
+      stiffness: 0.5,
+      length: 20,
+    };
+    this.sling = Constraint.create(options);
+    World.add(world, this.sling);
+  }
+
+  show() {
+    stroke(255);
+    const posA = this.sling.pointA;
+    const posB = this.sling.body.position;
+    line;
+  }
+}
